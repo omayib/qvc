148b348ca490caf49645ff0898d341197aa69b3e
akbarul <akbarul@qiscus.co>
2017-06-16 16:34:44 +0700

    add login manager

---
+++ QvcModul/qvclogin/qvclogin/QvcLoginManager.swift
@@ -0,0 +1,21 @@
+//
+//  QvcLoginManager.swift
+//  qvclogin
+//
+//  Created by qiscus on 6/16/17.
+//  Copyright © 2017 qiscus. All rights reserved.
+//
+
+import Foundation
+class QvcLoginManager{
+    public init(email:String) {
+        print("email")
+    }
+    public init(){
+        
+    }
+    
+    public func login(){
+        print("login")
+    }
+}
